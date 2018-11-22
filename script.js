bf299d9105c335f98f6e65960b6e754ce10dffaf
LeraZakharova <37138340+LeraZakharova@users.noreply.github.com>
2018-11-22 21:12:17 +0300

    update styles

--- script.js
+++ script.js
@@ -19,6 +19,14 @@ $(function() {
 	});
 });
 
+$(document).ready(function(){
+    $("#navigation").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 500);
+    });
+});
 
 function showHide(element_id) {
 	var	obj = document.getElementById(element_id);
